0922aff81165cca17bfd52b61067f11fa6522baf
Breno Masato Matayoshi <145488931+BrenoMatayoshi@users.noreply.github.com>
2025-01-24 14:21:37 -0300

    Parte da seção 1 e troca de cores

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,22 @@ function toggleMenu() {
     const checkbox = document.getElementById('menu-burguer');
     checkbox.checked = !checkbox.checked;
   }
+
+  const texts = ["my website!", "our platform!", "this page!"];
+  let textIndex = 0;
+  let charIndex = 0;
+
+  function typeEffect() {
+      if (charIndex < texts[textIndex].length) {
+          document.getElementById('typing').innerHTML = texts[textIndex].substring(0, charIndex + 1) + '<span class="cursor">|</span>';
+          charIndex++;
+          setTimeout(typeEffect, 100);
+      } else {
+          setTimeout(() => {
+              charIndex = 0;
+              textIndex = (textIndex + 1) % texts.length;
+              typeEffect();
+          }, 2500);
+      }
+  }
+  typeEffect();
